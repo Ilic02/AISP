85eae802ed4fbdf28f646a00a7b8e79886b370d5
Ilic02 <nikola.ni959@gmail.com>
2023-11-13 17:32:05 +0100

    Added Cas06

---
+++ Cas06/grejov_kod.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+string grejov_kod(unsigned n, unsigned k){
+    if(n == 0)
+        return "";
+    if(k < pow(2,n-1))
+        return "0" + grejov_kod(n-1,k);
+    else
+        return "1" + grejov_kod(n-1, pow(2,n) - 1 - k);
+}
+
+int main (){
+    unsigned n, k;
+    cin >> n >> k;
+
+    cout << grejov_kod(n,k) << endl;
+
+    return 0;
+}
